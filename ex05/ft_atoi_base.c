4d7c2e5bce450e7c5766a48e0d52f27e1d0ad0e4
iacopobru <iacmega@hotmail.it>
2024-11-20 15:43:26 +0100

    Update ft_atoi_base.c
    per verificare (federico)

--- ex05/ft_atoi_base.c
+++ ex05/ft_atoi_base.c
@@ -10,87 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	is_in_str(char *str, char c)
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int	i;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+	unsigned int	j;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == c)
-			return (i + 1);
-		i++;
-	}
-	return (0);
+	dest_len = 0;
+	src_len = 0;
+	j = 0;
+	while (dest[dest_len] != '\0')
+		dest_len++;
+	while (src[src_len] != '\0')
+		src_len++;
+	if (size <= dest_len)
+		return (size + src_len);
+	i = dest_len;
+	while (src[j] != '\0' && i < size - 1)
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (dest_len + src_len);
 }
 
-int	ft_atoi(char *str, char *base, int size_base)
+
+#include <stdio.h>
+#include <string.h>
+int main()
 {
-	int	result;
-	int	mult;
-	int	index;
+	char msg[] = "ciao hello helloo bonjure                                   ";
+	msg[23] = 0; 
+	char msg2[] = "prova";
+	printf("%d\n", strlcat(msg, msg2, 35));
+	//printf("%d\n", ft_strlcat(msg, msg2, 35));
 
-	mult = 1;
-	result = 0;
-	index = 0;
-	while (str[index] == ' ' || (str[index] >= 9 && str[index] <= 13))
-		index++;
-	while (str[index] == '-' || str[index] == '+')
-	{
-		if (str[index] == '-')
-			mult = -mult;
-		index++;
-	}
-	while (is_in_str(base, str[index]))
-	{
-		result = (result * size_base) + (is_in_str(base, str[index]) - 1);
-		index++;
-	}
-	return (result * mult);
+	printf("\n%s\n", msg);
+	printf("%s\n", msg2);
 }
-
-int	has_duplicates(char *str)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		j = i + 1;
-		while (str[j] != '\0')
-		{
-			if (str[i] == str[j])
-				return (1);
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
-
-int	ft_atoi_base(char *str, char *base)
-{
-	int	i;
-
-	i = 0;
-	while (base[i] != '\0')
-	{
-		if (base[i] == '+' || base[i] == '-')
-			return (0);
-		if (base[i] <= 32 || base[i] == 127)
-			return (0);
-		i++;
-	}
-	if (has_duplicates(base))
-		return (0);
-	if (i <= 1)
-		return (0);
-	return (ft_atoi(str, base, i));
-}
-
-// int main(){
-// 	char str[] = "1000000";
-// 	char base[] = "01";
-// 	printf("%d\n", ft_atoi_base(str, base));
-// }
